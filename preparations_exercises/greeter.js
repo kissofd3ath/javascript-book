6be86cec0c533063869fc107ea86a60c4f54f01b
kissofd3ath <rkiss369@gmail.com>
2022-05-08 01:44:28 -0400

    Variables Exercises

---
+++ preparations_exercises/greeter.js
@@ -0,0 +1,6 @@
+/* Program that writes three greetings using a variable assigned to name */
+
+let name = "Victor";
+console.log(`Good Morning, ${name}.`);
+console.log(`Good Afternoon, ${name}.`);
+console.log(`Good Evening, ${name}.`);
---
+++ preparations_exercises/pets.js
@@ -0,0 +1,8 @@
+let names = ['asta', 'butterscotch', 'pudding', 'neptune', 'darwin'];
+let pets = {
+  asta: 'dog',
+  butterscotch: 'cat',
+  pudding: 'cat',
+  neptune: 'fish',
+  darwin: 'lizard',
+};
