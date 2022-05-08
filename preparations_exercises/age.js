fdf19efa6ba7a08789c8c6923298c13bba2d2ef1
kissofd3ath <rkiss369@gmail.com>
2022-05-08 01:41:31 -0400

    Varibles Exercises

---
+++ preparations_exercises/age.js
@@ -0,0 +1,11 @@
+/* A program that includes a person's age */ 
+/* and then calculates and reports their future */
+/* age in 10, 20, 30 and 40 years. */
+
+let age = 27;
+
+console.log(`You are ${age} years old.`);
+console.log(`In 10 years, you will be ${age + 10} years old.`);
+console.log(`In 20 years, you will be ${age + 20} years old.`);
+console.log(`In 30 years, you will be ${age + 30} years old.`);
+console.log(`In 40 years, you will be ${age + 40} years old.`);
